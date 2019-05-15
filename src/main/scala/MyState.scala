8013cc1ed60b12ccc1ac9939b014d0c769976fb1
Yaroslav Dashkevich <yaroslav.dashkevich@gmail.com>
2019-05-15 09:58:59 +0700

    Add recursive render example

--- src/main/scala/MyState.scala
+++ src/main/scala/MyState.scala
@@ -3,14 +3,19 @@ import korolev.execution._
 import scala.concurrent.Future
 import korolev.state.javaSerialization._
 
+class XmlDoc(
+  val text: String,
+  val children: List[XmlDoc],
+) {}
+
 class MyMessage(
   val author: String,
   val text: String
 ) { }
 
 case class MyState(
-  name: String,
-  messages: List[MyMessage]
+  tab: Int,
+  xmls: List[XmlDoc],
 )
 
 object MyState {
--- src/main/scala/tabs.scala
+++ src/main/scala/tabs.scala
@@ -9,6 +9,7 @@ import korolev.state.javaSerialization._
 
 import scala.concurrent.Future
 import MyState.globalContext._
+import levsha.Document.Empty
 import symbolDsl._
 
 object tabs extends App {
@@ -22,75 +23,294 @@ object tabs extends App {
 
   val generateEmptyState = () => {
     new MyState(
-      name = "",
-      messages = List[MyMessage]()
+      tab = 0,
+      xmls = List(
+        new XmlDoc("item 1", List(
+          new XmlDoc("item 1_1", List()),
+          new XmlDoc("item 1_2", List(
+            new XmlDoc("item 1_2_1", List()),
+          )),
+        )),
+        new XmlDoc("item 2", List()),
+        new XmlDoc("item 3", List(
+          new XmlDoc("item 3_1", List()),
+        )),
+      ),
     )
   }
 
-  val renderLogin = (state: MyState) =>
+  object Columns extends Enumeration {
+    val CardNumber, FirstName, FathersName, LastName, SerPasp, NumPasp, RegNumber, BirthDate,
+    CbCode, Login, id, Password, ContrStatus, AddInfoCn, TB_Code, TB_OSB,
+    TB_Office, Phone = Value
+  }
+
+  def ModalCard(title: String,
+                display: String = "flex",
+                showCloseButton: Boolean = false,
+                style: Attr = Empty)(fields: Node*)(buttons: Node*) = {
     'div (
-      'form (
-        'input (
-          loginInputId,
-          'placeholder /= "Login"
+      'class /= "modal",
+      'style /= s"display: $display;", //
+      'div ('class /= "modal-background"),
+      'div (
+        'class /= "modal-card",
+        style, //
+        'header ('class /= "modal-card-head",
+          'p ('class /= "modal-card-title", title),
+          if (showCloseButton) 'a ( 'href /= "",'class /= "delete") else Empty
         ),
-        'button (
-          'type /= "submit",
-          "Login"
-        ),
-        event('submit) { access =>
-          access.property(loginInputId, 'value) flatMap( { value =>
-            access.transition {
-              _.copy(name = value)
-            }
-          })
-        }
+        'section ('class /= "modal-card-body", fields),
+        'footer ('class /= "modal-card-foot",
+          //'a ( 'class /= "button is-success", disabledFlag, "Login")
+          buttons)
       )
     )
+  }
 
-  val renderChat = (state: MyState) =>
-    Seq(
-      'p (
-        "Your name is " + state.name
-      ),
-      'div (
-        'index /= "messagesContainer",
-        state.messages map (msg => {
-          'div (
-            'class /= "messageText",
-            'style /= "padding: 10px",
-            msg.author + ": " + msg.text
-          )
-        })
-      ),
-      'div (
-        'index /= "inputWrapper",
-        'form (
-          'textarea (
-            inputId,
-            'style /= "display: block;"
-          ),
-          'button (
-            'type /= "submit",
-            "Send"
-          ),
-          event('submit) { access =>
-            val prop = access.property(inputId)
-            prop.get('value) flatMap { value =>
-              prop.set('value, "") flatMap { _ =>
-                access.transition { transition => {
-                    if (value.length > 0) {
-                      val msgs = state.messages :+ new MyMessage(state.name, value)
-                      transition.copy(messages = msgs)
-                    } else {
-                      transition
-                    }
-                  }
+  def TextFieldWithLabel(
+                          label: String,
+                          tpe: String = "text",
+                          value: String = null,
+                          floating: Boolean = false)(elementId: Node)(attr: Attr*) = {
+    val valueOpt = Option(value)
+    'div ('class /= "field",
+      'label ('class /= "label", label),
+      'p ('class /= "control",
+        'input ('type /= tpe,
+          'class /= "input",
+          valueOpt.map(s => 'value /= s),
+          elementId,
+          attr)))
+  }
+
+  def renderTabHeader(state: MyState) =
+    'div (
+      'class /= "tabs",
+      'ul (
+        'li (
+          'class /= (if (state.tab == 0) "is-active" else ""),
+          'a (
+            "Tab 1",
+            event('click) {
+              access => {
+                access.transition {
+                  transition => transition.copy(tab = 0)
                 }
               }
             }
-          }
+          )
+        ),
+        'li (
+          'class /= (if (state.tab == 1) "is-active" else ""),
+          'a (
+            "Tab 2",
+            event('click) {
+              access => {
+                access.transition {
+                  transition => transition.copy(tab = 1)
+                }
+              }
+            }
+          )
+        ),
+        'li (
+          'class /= (if (state.tab == 2) "is-active" else ""),
+          'a (
+            "Tab 3",
+            event('click) {
+              access => {
+                access.transition {
+                  transition => transition.copy(tab = 2)
+                }
+              }
+            }
+          )
         )
+      )
+    )
+
+
+  def renderTab1() =
+    'div (
+      'div (
+        'class /= "columns",
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Имя *", floating = true, tpe = "userID")(Empty)(
+            'name /= Columns.FirstName.toString,
+            'pattern /= "\\[a-z0-9._%]{1,20}", //a-z только строчные буквы. Возможно нужно разрешить прописные.
+            'required /= "",
+            'maxlength /= "20")
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Фамилия *", floating = true, tpe = "userID")(Empty)(
+            'name /= Columns.LastName.toString,
+            'pattern /= "\\[a-z0-9._%]{1,20}",
+            'required /= "",
+            'maxlength /= "20")
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Отчество", floating = true, tpe = "userID")(Empty)(
+            'pattern /= "\\[a-z0-9._%]{1,20}",
+            'name /= Columns.FathersName.toString,
+            'maxlength /= "20")
+        )
+      ),
+      'div (
+        'class /= "columns",
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Дата рождения *", floating = true, tpe = "date", value = "1972-01-30")(Empty)(
+            'name /= Columns.BirthDate.toString,
+            'required /= "")
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Серия паспорта", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.SerPasp.toString,
+            'pattern /= "\\d{1,4}",
+            'placeholder /= "xxxx",
+            'title /= "Поле должно иметь числовое значение",
+            'maxlength /= "4")
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Номер паспорта *", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.NumPasp.toString,
+            'pattern /= "\\d{1,6}",
+            'placeholder /= "xxxxxx",
+            'title /= "Поле должно иметь числовое значение",
+            'required /= "",
+            'maxlength /= "6")
+        ),
+        'div ('class /= "column",
+          TextFieldWithLabel("Телефон *", floating = true, tpe = "tel")(Empty)(
+            'name /= Columns.Phone.toString,
+            'required /= "",
+            'pattern /= "7\\d{10}",
+            'placeholder /= "7xxxxxxxxxx",
+            'maxlength /= "11"
+          )
+        )
+      ),
+      'div (
+        'class /= "columns",
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Логин *", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.Login.toString,
+            'pattern /= "\\d{10}",
+            'placeholder /= "1234567890",
+            'title /= "Поле должно состоять из 10-ти цифр.",
+            'required /= "",
+            'maxlength /= "10",
+            'style /= "width: 23%;")
+        )
+      ),
+      'div (
+        'class /= "columns",
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Номер карточки *", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.CardNumber.toString,
+            'pattern /= "\\d{16}",
+            'placeholder /= "xxxxxxxxxxxxxxxx",
+            'title /= "Поле должно иметь числовое значение",
+            'required /= "",
+            'maxlength /= "16",
+            'value /= "1234 5678 9012 3456"
+          )
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Тербанк *", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.TB_Code.toString,
+            'pattern /= "\\d{1,4}",
+            'placeholder /= "xxxx",
+            'title /= "Поле должно иметь числовое значение, например 99",
+            'required /= "",
+            'maxlength /= "4")
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("ОСБ *", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.TB_OSB.toString,
+            'pattern /= "\\d{1,4}",
+            'placeholder /= "xxxx",
+            'title /= "Поле должно иметь числовое значение, например 9038",
+            'required /= "",
+            'maxlength /= "4")
+        ),
+        'div (
+          'class /= "column",
+          TextFieldWithLabel("Код офиса *", floating = true, tpe = "text")(Empty)(
+            'name /= Columns.TB_Office.toString,
+            'pattern /= "\\d{1,7}",
+            'placeholder /= "xxxxxxx",
+            'title /= "Поле должно иметь числовое значение, например 16",
+            'required /= "",
+            'maxlength /= "7")
+        )
+      )
+    )
+
+  def renderTab2(nodes: List[XmlDoc]) =
+    'div (
+      nodes.map(renderXmlNode)
+    )
+
+  def renderTab3() =
+    'div (
+      "Tab 3"
+    )
+
+  def renderXmlNode(doc: XmlDoc) : levsha.Document.Node[Context.Effect[Future, MyState, Any]] =
+    'div(
+      doc.text,
+      'div(
+        'class /= "xml-children-container",
+        doc.children.map(renderXmlNode),
+      ),
+    )
+
+  def showFormNewClient(state: MyState) =
+    'form (
+      ModalCard("Ввод нового клиента", "flex", true, 'style /= "width: 840px;")(
+        renderTabHeader(state),
+        state.tab match {
+          case 0 => renderTab1()
+
+          case 1 => renderTab2(state.xmls)
+
+          case 2 => renderTab3()
+        }
+      )(
+        'div (
+          'class /= "field is-grouped",
+          'div (
+            'class /= "field is-grouped",
+            'p ('class /= "control",
+              'button ('type /= "submit", 'class /= "button is-success", "Создать")),
+            'p ('class /= "control",
+              'a ('href /= "", 'class /= "button is-danger", "Отмена"))
+          )
+        )
+      )
+    )
+
+  val head =
+    Seq(
+      'meta('charset /= "utf-8"),
+      'meta('httpEquiv /= "X-UA-Compatible", 'content /= "IE=edge"),
+      'meta('name /= "viewport", 'content /= "width=device-width, initial-scale=1"),
+      'link('rel /= "stylesheet", 'href /= "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css"),
+      // in hardcode we trust!
+      'style(
+        'type /= "text/css",
+        ".xml-children-container { padding-left: 10px; }"
       )
     )
 
@@ -98,22 +318,14 @@ object tabs extends App {
     stateStorage = StateStorage.default(generateEmptyState()),
     router = emptyRouter,
     render = {
-      case state: MyState => {
-        'body (
-          if (state.name == "") {
-            renderLogin(state)
-          } else {
-            renderChat(state)
-          }
-        )
-      }
+      case state: MyState =>
+          'body (
+            showFormNewClient(state)
+          )
+    },
+    head = {
+      head
     }
-  )
-
-  val head = Seq(
-    'meta('charset /= "utf-8"),
-    'meta('httpEquiv /= "X-UA-Compatible", 'content /= "IE=edge"),
-    'meta('name /= "viewport", 'content /= "width=device-width, initial-scale=1"),
   )
 
   private val route = akkaHttpService(config).apply(AkkaHttpServerConfig())
